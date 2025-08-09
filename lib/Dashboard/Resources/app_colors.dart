2ca0daa4942939b4659afc84c043e45e077342c5
Qamrosh Ali Nazar <qamroshalinazar55@outlook.com>
2025-08-09 14:04:02 +0500

    Added theme and colors

---
+++ lib/Dashboard/Resources/app_colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xfff59c24);
+  static const Color primaryBackgroundColor = Color(0xff111111);
+  static const Color lightPurple = Color(0xffD896FF);
+  static const grey = Color(0xff6c6b6b);
+  static const darkGrey = Color(0xff444343);
+  static const black = Color(0xff000000);
+  static const lightGrey = Color(0xffBDBDBD);
+  static const red = Color(0xffF44336);
+  static const lightRed = Color(0xfff9e7e5);
+  static const white = Color(0xffffffff);
+  static const textFieldBorder = Color(0xffaeb0af);
+  static const veryLightGray = Color(0xfff4f5f4);
+}
---
+++ lib/Dashboard/Resources/app_theme.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+final ThemeData appTheme = ThemeData(
+  primaryColor: AppColors.primaryColor,
+  scaffoldBackgroundColor: AppColors.primaryBackgroundColor, // Background color for screens
+  dialogBackgroundColor: Colors.white,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.white,
+    elevation: 0,
+  ),
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    primary: AppColors.primaryColor,
+  ),
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.white,
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+    ),
+    bodyMedium: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.white,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+    ),
+    bodySmall: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.white,
+      fontSize: 12,
+      fontWeight: FontWeight.w600,
+    ),
+    labelSmall: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.white,
+      fontSize: 10,
+      fontWeight: FontWeight.w700,
+    ),
+    labelMedium: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.white,
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+    ),
+    headlineLarge: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.white,
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.white,
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.white,
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+    ),
+
+    // Define other text styles as needed
+  ),
+  iconTheme: const IconThemeData(
+    color: AppColors.darkGrey,
+  ),
+  inputDecorationTheme: const InputDecorationTheme(
+    hintStyle: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.lightGrey,
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+    ),
+    errorStyle: TextStyle(
+      fontFamily: 'Poppins',
+      color: AppColors.red,
+      fontSize: 15,
+      fontWeight: FontWeight.w400,
+    ),
+    border: UnderlineInputBorder(
+      borderSide: BorderSide(color: AppColors.textFieldBorder),
+    ),
+    enabledBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: AppColors.textFieldBorder),
+    ),
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: AppColors.primaryColor),
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: WidgetStateProperty.all(AppColors.primaryColor),
+      foregroundColor: WidgetStateProperty.all(AppColors.white),
+      padding: WidgetStateProperty.all(const EdgeInsets.symmetric(vertical: 16, horizontal: 24)),
+      textStyle: WidgetStateProperty.all(
+        const TextStyle(
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.w800,
+          fontSize: 18,
+        ),
+      ),
+      shape: WidgetStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(28),
+        ),
+      ),
+    ),
+  ),
+  extensions: const <ThemeExtension<dynamic>>[
+    CustomTextStyleExtension(
+      boldDarkGreyLarge: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 18,
+        color: AppColors.darkGrey,
+      ),
+    ),
+  ],
+);
+
+@immutable
+class CustomTextStyleExtension extends ThemeExtension<CustomTextStyleExtension> {
+  final TextStyle? boldDarkGreyLarge;
+
+  const CustomTextStyleExtension({this.boldDarkGreyLarge});
+
+  @override
+  CustomTextStyleExtension copyWith({TextStyle? boldDarkGreyLarge}) {
+    return CustomTextStyleExtension(
+      boldDarkGreyLarge: boldDarkGreyLarge ?? this.boldDarkGreyLarge,
+    );
+  }
+
+  @override
+  CustomTextStyleExtension lerp(ThemeExtension<CustomTextStyleExtension>? other, double t) {
+    if (other is! CustomTextStyleExtension) return this;
+    return CustomTextStyleExtension(
+      boldDarkGreyLarge: TextStyle.lerp(boldDarkGreyLarge, other.boldDarkGreyLarge, t),
+    );
+  }
+}
